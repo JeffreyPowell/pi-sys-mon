40ebc812ec3040334bc42b9e3c8b7cd3374eec97
Jeff <Jffrypwll@googlemail.com>
2014-12-04 14:02:52 +0000

    Update poller-wrapper.sh
    removed extra forward slash

--- poller-wrapper.sh
+++ poller-wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/usr//bin/python /usr/local/scripts/git/pi-adc-mon/poll-adc-volts.py
+/usr/bin/python /usr/local/scripts/git/pi-adc-mon/poll-adc-volts.py
 /bin/bash /usr/local/scripts/git/pi-adc-mon/chart-adc-volts.sh
 
