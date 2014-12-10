abeb273af66b78b9b51d1f150a48f03e21e89984
Jeff <Jffrypwll@googlemail.com>
2014-12-10 14:32:39 +0000

    Update poller-wrapper.sh

--- poller-wrapper.sh
+++ poller-wrapper.sh
@@ -3,3 +3,4 @@
 /usr/bin/python /usr/local/scripts/git/pi-adc-mon/poll-adc-volts.py
 /bin/bash /usr/local/scripts/git/pi-adc-mon/chart-adc-volts.sh
 
+/bin/bash /usr/local/scripts/git/pi-sys-mon/pollers/poll-sys-uptime.sh
