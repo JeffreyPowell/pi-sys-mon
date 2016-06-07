4497ce3f36f91972299b48e16abc5bc6231f13f1
pi@rpi-van <jffrypwll@googlemail.com>
2016-06-07 01:09:42 +0100

    corrected file paths

--- www/sys-cpuload.php
+++ www/sys-cpuload.php
@@ -1,18 +1,18 @@
 <?php
 
-create_graph("images/cpu-load-hour.png",  "-1h", "CPU Load last hour");
-create_graph("images/cpu-load-day.png",   "-1d", "CPU Load last day");
-create_graph("images/cpu-load-week.png",  "-1w", "CPU Load last week");
-create_graph("images/cpu-load-month.png", "-1m", "CPU Load last month");
+create_graph("images/cpuload-hour.png",  "-1h", "CPU Load last hour");
+create_graph("images/cpuload-day.png",   "-1d", "CPU Load last day");
+create_graph("images/cpuload-week.png",  "-1w", "CPU Load last week");
+create_graph("images/cpuload-month.png", "-1m", "CPU Load last month");
 
 echo "<html><head>";
 #echo "<style> div.outer {display:block; margin-left:auto; margin-right:auto;}</style>";
 echo "<meta http-equiv=\"refresh\" content=\"30\">";
 echo "</head><body bgcolor='#000000'>";
-echo "<img src='images/cpu-load-hour.png'  border ='5'>";
-echo "<img src='images/cpu-load-day.png'   border ='5'>";
-echo "<img src='images/cpu-load-week.png'  border ='5'>";
-echo "<img src='images/cpu-load-month.png' border ='5'>";
+echo "<img src='images/cpuload-hour.png'  border ='5'>";
+echo "<img src='images/cpuload-day.png'   border ='5'>";
+echo "<img src='images/cpuload-week.png'  border ='5'>";
+echo "<img src='images/cpuload-month.png' border ='5'>";
 echo "</body></html>";
 
 exit;
@@ -27,7 +27,7 @@ function create_graph($output, $start, $title) {
     "--lower=0",
     "--height=200",
     "--width=370",
-    "DEF:a=/home/pi/bin/sys/data/data_cpu_load.rrd:load:AVERAGE",
+    "DEF:a=/home/pi/bin/sys/data/sys-cpuload.rrd:data:AVERAGE",
     "CDEF:b=a,1,*",
     "LINE1:b#FF0000:CPU load",
  #   "AREA:b#AAFFAA",
